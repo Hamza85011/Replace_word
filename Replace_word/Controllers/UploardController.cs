f79cd492a14065cf44cc3cff62885c79a99e7328
hamza khan <hamza.khan@aircod.com>
2024-07-11 15:56:54 +0500

    Add project files.

---
+++ Replace_word/Controllers/UploardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class UploadController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Process(IFormFile file)
+    {
+        if (file != null && file.Length > 0)
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var content = reader.ReadToEnd();
+                var updatedContent = content.Replace("T", "C");
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", file.FileName);
+                System.IO.File.WriteAllText(filePath, updatedContent);
+            }
+            TempData["Message"] = "File processed and saved successfully.";
+        }
+        else
+        {
+            TempData["InsertMsg"] = "Failed to save data.";
+        }
+        return View("Index");
+    }
+}
